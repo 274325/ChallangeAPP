916f9d38120109aa326f6e9030591f7084e32fec
274325 <dawid.siewert@wp.pl>
2023-02-21 21:47:20 +0100

    dzien 4

---
+++ ChalangeAPP/ChalangeAPP/Program.cs
@@ -0,0 +1,18 @@
+﻿// zadanie dzień 4 :)
+
+int Age = 15;
+string Name = "Ewa";
+bool kobieta = false;
+
+if (Name == "Ewa" && Age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (Age < 33 && kobieta)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (Age < 18 && !kobieta)
+{
+    Console.WriteLine("mężczyzna niepełnoletni");
+}
